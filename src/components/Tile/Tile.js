b09e64f1d0f3d6b361e821c6c324aef3fc5f6faf
Chalida Pisuraj <pisurajc@uci.edu>
2020-12-03 20:18:55 -0800

    Fix text size

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -9,7 +9,7 @@ class Tile extends React.Component{
                 <div className="image-container">
                     <a href={this.props.path}><img className="picture" src={this.props.tileImageSrc} /></a>
                 </div>
-                <a className="name" href={this.props.path}>{this.props.name}</a>
+                <a className="tileName" href={this.props.path}>{this.props.name}</a>
             </div>
         )
     }
