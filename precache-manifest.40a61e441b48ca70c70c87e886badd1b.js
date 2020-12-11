8b65f82d42baf1264a920665cdfb5005d5f97566
Chalida Pisuraj <pisurajc@uci.edu>
2020-12-10 21:09:08 -0800

    First new build

---
+++ precache-manifest.40a61e441b48ca70c70c87e886badd1b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7228af88ba597359bb08b7868a1ea8ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ab029d6c3712234d1dd",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "03386969c1749e7c742a",
+    "url": "/static/css/main.3e268e7e.chunk.css"
+  },
+  {
+    "revision": "8ab029d6c3712234d1dd",
+    "url": "/static/js/2.234790ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.234790ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03386969c1749e7c742a",
+    "url": "/static/js/main.522b138a.chunk.js"
+  },
+  {
+    "revision": "8acade14f6ff4d58e068",
+    "url": "/static/js/runtime-main.11babfdf.js"
+  },
+  {
+    "revision": "3823ee0c0bddfd43df1d53202a89e097",
+    "url": "/static/media/Chalida_Pisuraj_Resume-10232020.3823ee0c.pdf"
+  },
+  {
+    "revision": "649bea721c4f3706b761de27b8c6b815",
+    "url": "/static/media/CoreLogic.649bea72.png"
+  },
+  {
+    "revision": "4541c860de41f92e69dfdc346f72f43b",
+    "url": "/static/media/Panasonic.4541c860.png"
+  },
+  {
+    "revision": "60afb2c01fdd1117f7cd23d6f4669328",
+    "url": "/static/media/SeniorDesign.60afb2c0.pdf"
+  },
+  {
+    "revision": "788272c9322090d34540e240b1174b07",
+    "url": "/static/media/SeniorDesign.788272c9.mp4"
+  },
+  {
+    "revision": "6f5c04b026cd44bb7bfc1bd83b4920a9",
+    "url": "/static/media/Tinder.6f5c04b0.png"
+  },
+  {
+    "revision": "f4fec5b47ec6112858e2924315c5034a",
+    "url": "/static/media/about.f4fec5b4.png"
+  },
+  {
+    "revision": "c884f427a7f5b780545f8a264cdc8688",
+    "url": "/static/media/check.c884f427.svg"
+  },
+  {
+    "revision": "03fed264b0e37b0ffbd60778e73cf74e",
+    "url": "/static/media/core_1.03fed264.png"
+  },
+  {
+    "revision": "23c8760527e6157b01c237c590601d80",
+    "url": "/static/media/github.23c87605.svg"
+  },
+  {
+    "revision": "784269eba1c2f4cb291a9d7b8d064223",
+    "url": "/static/media/linkedin.784269eb.svg"
+  },
+  {
+    "revision": "4d7dc504d1680a67ac85af990badd98e",
+    "url": "/static/media/market_1.4d7dc504.png"
+  },
+  {
+    "revision": "c2c2e835954c0e452e43e3784a77c642",
+    "url": "/static/media/market_2.c2c2e835.png"
+  },
+  {
+    "revision": "409a89a048b15c1fc30ae4a32a11f664",
+    "url": "/static/media/me.409a89a0.png"
+  },
+  {
+    "revision": "cec98f9253019cf6f3d66f5a01ca4ebd",
+    "url": "/static/media/panasonic_highlight.cec98f92.png"
+  },
+  {
+    "revision": "7507baff7693b8a3852e0e082c46248e",
+    "url": "/static/media/profile.7507baff.png"
+  },
+  {
+    "revision": "2292afdbd3b6da3575e85daeab21a660",
+    "url": "/static/media/side.2292afdb.png"
+  },
+  {
+    "revision": "82d68afb892b74937c2c1a6cbcb659c9",
+    "url": "/static/media/spotify.82d68afb.svg"
+  },
+  {
+    "revision": "8f0f677ef9abfcd00c5ff59f81922e9f",
+    "url": "/static/media/tinder_1.8f0f677e.png"
+  },
+  {
+    "revision": "ce122cd93362d40f67fb5a917c524855",
+    "url": "/static/media/tinder_3.ce122cd9.png"
+  },
+  {
+    "revision": "c3cb7f2f71d7be9a28eef0855be82c59",
+    "url": "/static/media/tinder_4.c3cb7f2f.png"
+  },
+  {
+    "revision": "b07ae53d5e43488e245e55a665349bc4",
+    "url": "/static/media/tinder_draft_3.b07ae53d.png"
+  },
+  {
+    "revision": "a25f2c6554bc5c14c9a323536101f08d",
+    "url": "/static/media/tinder_draft_4.a25f2c65.png"
+  },
+  {
+    "revision": "4d3545792ffecc2d031f3f55769d7039",
+    "url": "/static/media/tinder_preview2.4d354579.png"
+  }
+]);
