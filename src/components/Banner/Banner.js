7fdbaf9a6cfaa470ce2ac607f5fa8a1d5d7c381a
Chalida Pisuraj <pisurajc@uci.edu>
2020-12-10 16:34:49 -0800

    Fixed mobile resize issue

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -11,9 +11,11 @@ class Banner extends React.Component {
             <h2 className="thisIs">This Is {this.props.thisIs}</h2>
             <p className="info">{this.props.descript}</p>
             <button className="resume"><a className="resumeText" href={require("../images/Chalida_Pisuraj_Resume-10232020.pdf")}> <b>RESUME</b> </a></button>
-            <a href="https://www.linkedin.com/in/chalida-pisuraj/"><img className="icons" src={require("../images/linkedin.svg")}/></a>
-            <a href="https://github.com/chalidapisuraj"><img className="icons" src={require("../images/github.svg")}/></a>
-            <a href="https://open.spotify.com/playlist/37i9dQZF1EpjwqrA4q0iU9?si=oT90uQrLQq2qPXiFAyvbzA"><img className="icons" src={require("../images/spotify.svg")}/></a>
+            <div className="iconList">
+                <a href="https://www.linkedin.com/in/chalida-pisuraj/"><img className="icons" src={require("../images/linkedin.svg")}/></a>
+                <a href="https://github.com/chalidapisuraj"><img className="icons" src={require("../images/github.svg")}/></a>
+                <a href="https://open.spotify.com/playlist/37i9dQZF1EpjwqrA4q0iU9?si=oT90uQrLQq2qPXiFAyvbzA"><img className="icons" src={require("../images/spotify.svg")}/></a>
+            </div>
         </div>
         )
     }
